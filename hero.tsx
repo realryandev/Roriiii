17514ebbdc863ddc90db4ec2cfcc1d5dc630753d
Ryan <realryandev@gmail.com>
2025-05-18 00:34:51 +0200

    Delete hero.tsx

--- hero.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Hero() {
-    return (
-        <section className="w-full h-[80vh] relative overflow-hidden">
-            <Image src="/rori.jpg" alt="Rori" fill priority className="object-cover" />
-        </section>
-    )
-}
