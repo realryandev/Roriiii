de988b482dca782de6d0bbe1b9c849ddb492a325
Ryan <realryandev@gmail.com>
2025-05-18 14:43:28 +0200

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,19 @@ import Hero from "@/components/hero";
 import Paragraph from "@/components/paragraph";
 import Gallery from "@/components/gallery";
 import { FloatingAnimals } from "@/components/floating-animals";
+import Affirmations from "@/components/Affirmations";
+import BackgroundMusic from "@/components/BackgroundMusic";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-b from-[#fc94af] to-[#f25278]">
+      <BackgroundMusic />
       <FloatingAnimals />
       <Hero />
       <Paragraph />
       <Gallery />
+      <Affirmations />
     </main>
   );
 }
