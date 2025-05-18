81fc17970e476f561a712d252299a939aa9d8f19
Ryan <realryandev@gmail.com>
2025-05-18 14:53:35 +0200

    Add files via upload

--- src/components/paragraph.tsx
+++ src/components/paragraph.tsx
@@ -2,7 +2,7 @@ export default function Paragraph() {
     return (
         <section className="py-16 px-4 md:p-8 max-w-4xl mx-auto mb-6">
             <div className="bg-white/60 backdrop-blur-sm rounded-xl p-8 shadow-md">
-                <h2 className="text-3xl md:text-4xl mb-6 text-gray-400">Welcome to Rori</h2>
+                <h2 className="text-3xl md:text-4xl mb-6 dark:text-white">Welcome to Rori</h2>
                 <p className="text-lg text-gray-700 mb-4">Hey bestie! 💕 This website is like a super cute Pinterest board, but it&apos;s all about Rori! 💖 We&apos;re gonna fill it with all her amazing pictures, showing off her style, her art, and all the fun things she does. 📸 It&apos;s basically a sparkly place to see how awesome she is! 🌟</p>
                 <p className=" text-lg text-gray-700">Think of it as a happy place filled with Rori&apos;s vibes! 🌸 You&apos;ll see her cute outfits 👗, her creative projects 🎨, and all the sweet moments that make her special. 🥰 We want everyone to see how much she shines! ✨ So, get ready for a dose of pure Rori magic! 🌈</p>
             </div>
